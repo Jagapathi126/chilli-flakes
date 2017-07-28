1f05a97a88b497210cc3e178b0147e7a35b007bb
Kisalay Kumar <krisalay@outlook.com>
2017-07-28 19:17:13 +0530

    configuration class

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,28 @@
-class Configuration {
-  constructor(){
+let joi = require('joi');
 
+const configurationSchema = joi.object().keys({
+  dbUrl: joi.string().required(),
+  dbType: joi.string().required().lowercase(),
+  tableName: joi.string().empty('').default('users'),
+  formInputs: joi.any()
+})
+
+class Configuration {
+  constructor(config){
+    const {error, value} = joi.validate(config, configurationSchema);
+    if(error){
+      console.log(error);
+    }else{
+      console.log(value);
+    }
   }
 }
+
+interface IConfiguration {
+  dbUrl: string,
+  dbType: string,
+  tableName: string,
+  formInputs: any
+}
+
+let a = new Configuration({dbUrl: "Hello", dbType:"HelloType", formInputs:{"a":"a"}});
